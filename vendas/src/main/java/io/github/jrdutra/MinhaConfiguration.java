d1a923eca00683c70b78c30c709b6670c51c5777
jrdutra <jrdutra.com.br@gmail.com>
2020-07-09 14:35:55 -0300

    Vendas

--- vendas/src/main/java/io/github/jrdutra/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/jrdutra/MinhaConfiguration.java
@@ -1,14 +1,16 @@
 package io.github.jrdutra;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public  String applicationName(){
-        return "Sistema de Vendas";
+    @Bean()
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
     }
 
 }
