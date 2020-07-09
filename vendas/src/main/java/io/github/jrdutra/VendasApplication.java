633f6cb9ffef77bb8a85326c3fd4c60b5f890676
jrdutra <jrdutra.com.br@gmail.com>
2020-07-09 15:02:11 -0300

    Criando modelo

--- vendas/src/main/java/io/github/jrdutra/VendasApplication.java
+++ vendas/src/main/java/io/github/jrdutra/VendasApplication.java
@@ -1,28 +1,10 @@
 package io.github.jrdutra;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-    //Configuraçao por BEAN
-    //@Autowired
-    //@Qualifier("applicationName")
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
