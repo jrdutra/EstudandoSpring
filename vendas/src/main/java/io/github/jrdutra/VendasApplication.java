35e9bfe90a76986f8e5ce018188ffe9e09832e13
jrdutra <jrdutra.com.br@gmail.com>
2020-07-10 14:16:33 -0300

    Criando modelo

--- vendas/src/main/java/io/github/jrdutra/VendasApplication.java
+++ vendas/src/main/java/io/github/jrdutra/VendasApplication.java
@@ -1,10 +1,29 @@
 package io.github.jrdutra;
 
+import ch.qos.logback.core.net.server.Client;
+import io.github.jrdutra.domain.entity.Cliente;
+import io.github.jrdutra.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Joao"));
+            clientes.salvar(new Cliente("Joao2"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
