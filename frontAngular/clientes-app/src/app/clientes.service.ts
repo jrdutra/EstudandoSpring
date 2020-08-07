01d91382462369ee11b37ec9e8f1be97f80e7527
jrdutra <jrdutra.com.br@gmail.com>
2020-08-07 16:05:44 -0300

    Cadastro clientes (angular)

---
+++ frontAngular/clientes-app/src/app/clientes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './clientes/cliente';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor( private http: HttpClient) {
+    
+   }
+
+  salvar(cliente: Cliente): Observable<Cliente>{
+      return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
+
+  getCliente() : Cliente {
+    let cliente : Cliente = new Cliente();
+    cliente.nome = 'João';
+    cliente.cpf = '35643564536'
+    return cliente;
+  }
+
+}
--- frontAngular/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontAngular/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente } from '../cliente'
+import { ClientesService } from '../../clientes.service'
+
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente;
+  success: boolean = false;
+  errors: String[];
+
+  constructor( private service : ClientesService) { 
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.service
+      .salvar(this.cliente)
+      .subscribe(response => {
+        this.success = true;
+        this.errors = null;
+        this.cliente = response;
+      }, errorResponse =>{
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+      });
   }
 
 }
