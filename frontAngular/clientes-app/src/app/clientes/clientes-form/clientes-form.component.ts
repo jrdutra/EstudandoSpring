5ba5e30d6e104da6ac9fac9b62b76ea6ae4cb7b7
jrdutra <jrdutra.com.br@gmail.com>
2020-08-10 09:03:38 -0300

    Spring-Angular

--- frontAngular/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontAngular/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute, Params } from '@angular/router'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -13,16 +14,40 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: String[];
+  id : number;
+  //params: Observable<Params>;
 
-  constructor( private service : ClientesService, private router : Router) { 
+  constructor( 
+    private service : ClientesService, 
+    private router : Router,
+    private activatedRoute : ActivatedRoute) { 
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params = this.activatedRoute.params;
+
+    if(params && params['value'] && params['value']['id']){
+        this.id = params['value']['id'];
+        this.service.getClienteById(this.id).subscribe(
+          response => this.cliente = response,
+          errorResponse => this.cliente = new Cliente()
+        );
+    }
   }
 
   onSubmit(){
-    this.service
+
+    if(this.id){
+      this.service.atualizar(this.cliente)
+      .subscribe(response=>{
+        this.success = true;
+        this.errors = null;
+      }, errorResponse => {
+        this.errors = ['Erro ao atualizar o cliente.'];
+      });
+    }else{
+      this.service
       .salvar(this.cliente)
       .subscribe(response => {
         this.success = true;
@@ -32,6 +57,7 @@ export class ClientesFormComponent implements OnInit {
         this.success = false;
         this.errors = errorResponse.error.errors;
       });
+    }
   }
 
   voltarParaListagem(){
